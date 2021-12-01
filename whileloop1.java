b1bb8da45555380012e18c55c0214b27220c0078
talib.cpp <talibffdc@gmail.com>
2021-12-02 00:35:08 +0530

    programmss

---
+++ whileloop1.java
@@ -0,0 +1,11 @@
+public class whileloop1 {
+    public static void main(String []args){
+
+        int i=0;
+        while(i<11){
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
