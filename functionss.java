26549848da1b9ec95278d5fcb0369c8cc27f1a7e
talib.cpp <talibffdc@gmail.com>
2022-07-03 19:59:12 +0530

    twonumbersmultiply

---
+++ functionss.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class functionss {
+ 
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("Product of two numbers"+ " "+ MultiplyOfTwoNumber(a,b));
+
+    }
+    public static int MultiplyOfTwoNumber(int a, int b){
+        return a*b;
+     }
+    }
+
+ 
---
+++ practicetoday.java
@@ -0,0 +1,9 @@
+public class practicetoday {
+    public static (String [] args){
+        System.out.println("hello bro So start Noww");
+        for (int i=1; i<=5; i++){
+            for(int j=)
+        }
+    }
+    
+}
