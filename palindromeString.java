b1298962bb7bff5f6d0e5b4b3e6ff145bb98a1dd
talib.cpp <talibffdc@gmail.com>
2023-01-20 22:38:21 +0530

    string check is palindrome or not

---
+++ palindromeString.java
@@ -0,0 +1,30 @@
+public class palindromeString {
+
+    public static void ispalindrome(String str){
+        StringBuilder sb= new StringBuilder(str);
+
+            sb.reverse();
+            String rev=sb.toString();
+             
+            if(str.equals(rev)){
+               System.out.println("palindrome");
+            
+            }
+            else {
+                System.out.println("not palindorme");
+
+            }
+
+        }
+
+        public static void main(String[] args) {
+            String str="talib";
+            ispalindrome(str); 
+
+        }
+    
+    }
+
+
+
+
