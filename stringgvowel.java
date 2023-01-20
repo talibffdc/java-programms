168f7f9a9ed1d1ca0f2a50bd880e2304ea8a9e1e
talib.cpp <talibffdc@gmail.com>
2023-01-20 20:46:05 +0530

    vowel find and find the index number of vowel

---
+++ stringgvowel.java
@@ -0,0 +1,18 @@
+public class stringgvowel {
+
+
+    public static void main(String[] args) {
+
+
+        String str=new String("hi welcome to my world");
+
+        for(int i=0; i<str.length(); i++){
+            if( str.charAt(i)=='a' || str.charAt(i)=='e' || str.charAt(i)=='i' || str.charAt(i)=='o'|| str.charAt(i)=='u'){
+            System.out.println("given string contains " + str.charAt(i) + " at the index "+ i);
+        }
+        
+    }
+    
+}
+    
+}
