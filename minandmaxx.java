025a1673347c4c31cdc5958916f2351598230c38
talib.cpp <talibffdc@gmail.com>
2023-02-28 20:59:38 +0530

    find the min and max ELEMENT in the one code.

--- minandmaxx.java
+++ minandmaxx.java
@@ -2,17 +2,26 @@ public class minandmaxx {
 
     public static void main(String[] args) {
         int arr[] ={1, 2, 4, 6, 8, 46};
-        int max= arr[0];
-
+        int max= Integer.MIN_VALUE;
+        int min=Integer.MAX_VALUE;
         
+
+        for (int i=1; i<arr.length; i++){
+            if(min>arr[i]){
+                min=arr[i];
+            }
+
+        }
+        System.out.println("the min element is "+""+min);
 
         for (int i=1; i<arr.length; i++){
             if(max<arr[i]){
                 max=arr[i];
-                
             }
 
         }
+         
+       
         System.out.println("the max element is "+""+max);
     }
     
