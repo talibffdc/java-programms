12215b514831ad1a919b185ffbdb6226d07d98b6
talib.cpp <talibffdc@gmail.com>
2023-02-28 21:20:04 +0530

    reverse the array

---
+++ reversee.java
@@ -0,0 +1,10 @@
+public class reversee {
+    public static void main(String[] args) {
+        int arr[]= {1,2,3,5,55,7,89,};
+      
+        for (int i=arr.length-1; i>0; i--){
+            System.out.print(arr[i]+",");
+        }
+    }
+    
+}
