53febc47729b35897b921c8d26bcd07fd43d2583
talib.cpp <talibffdc@gmail.com>
2023-03-03 00:07:41 +0530

    practice

---
+++ solu.java
@@ -0,0 +1,12 @@
+public class solu {
+
+    public static void main(String[] args){
+        System.out.println("ksjsjs");
+        int i;
+        for ( i=1; i<6; i++){
+            if (i>3)continue;
+        }
+        System.out.println(i);
+        }  
+    
+}
